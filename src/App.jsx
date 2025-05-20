60e2b61009846eef225358db4b77f1a1ac99f450
o-tushan <owentushan@icloud.com>
2025-05-20 14:57:22 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,297 @@
+import React, {useState} from 'react';
+import './App.css'
+import data from './intern_project_data.json'
+import logo from './logo.jpg'
+import {
+    TableBody, TableCell, TableContainer, TableHead, TableRow, Paper,
+    Drawer, Typography, IconButton, Box, AppBar, Toolbar, Button
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+
+const draftData = data.bio
+const scoutData = data.scoutRankings
+const gameData = data.seasonLogs
+const rankingFields = ["ESPN Rank", "Sam Vecenie Rank", "Kevin O'Connor Rank", "Kyle Boone Rank", "Gary Parrish Rank"]
+const reportData = data.scoutingReports
+
+
+const calculateAge = (birthDate) => {
+    const birth = new Date(birthDate)
+    const today = new Date()
+
+    let age = today.getFullYear() - birth.getFullYear()
+    const m = today.getMonth() - birth.getMonth()
+    if (m < 0 || (m === 0 && today.getDate() < birth.getDate())) {
+        age -= 1
+    }
+    return age
+}
+
+
+function App() {
+  /*const [count, setCount] = useState(0)*/
+
+  return (
+      <div className = "App">
+          <DraftBoard />
+      </div>
+  )
+}
+
+
+function DraftBoard() {
+    const [selectedPlayer, setSelectedPlayer] = useState(null);
+    const handleClick = (player) => {
+        setSelectedPlayer(player);
+    };
+    const handleClose = () => {
+        setSelectedPlayer(null);
+    };
+    const matchingPlayer = scoutData.find(stat => stat.playerId === selectedPlayer?.playerId);
+    const matchingPlayerGS = gameData.find(stat => stat.playerId === selectedPlayer?.playerId && stat.League === "NCAA");
+    const rankingValues = rankingFields.map(field => matchingPlayer?.[field]).filter(val => val != null && !isNaN(val))
+    const prospectRanking = rankingValues.length > 0 ? rankingValues.reduce((sum, val) => sum + val) / rankingValues.length : null
+    const matchingReports = reportData.filter(stat => stat.playerId === selectedPlayer?.playerId)
+
+    return (
+        <>
+            {/*Top Bar with buttons*/}
+            <AppBar position = "static" color = "inherit" sx = {{mb: 3}}>
+                <Toolbar>
+                    <Typography variant = "h6" sx = {{flexGrow : 1}}>
+                        Dallas Mavericks Draft Board
+                    </Typography>
+                    <Button color = "inherit" onClick = {() => alert("Feature in progress")}>
+                        Add New Report
+                    </Button>
+                    <Button color = "inherit" onClick = {() => alert("Feature in progress")}>
+                        Filter players
+                    </Button>
+                </Toolbar>
+            </AppBar>
+
+            <Box sx={{ maxWidth: '800px', pb: 6, margin: '0 auto', mt: 3}}>
+            <h1 style = {{textAlign : 'center'}}>2025 Draft Big Board</h1>
+                <TableContainer component = {Paper}>
+                    <table className = "draft-board">
+                        <TableHead style={{backgroundColor: '#E8E8E8'}}>
+                            <TableRow>
+                                <TableCell sx={{ width: '10%'}}>Overall ranking</TableCell>
+                                <TableCell sx={{ width: '10%'}}>Name</TableCell>
+                                <TableCell sx={{ width: '30%'}}>Age</TableCell>
+                                <TableCell sx={{ width: '30%'}}>Height</TableCell>
+                                <TableCell sx={{ width: '20%'}}>Current Team</TableCell>
+                            </TableRow>
+                        </TableHead>
+
+                        <TableBody>
+                            {draftData.map((player, index) => (
+                            <TableRow key={index} hover onClick={() => handleClick(player)} style={{cursor: 'pointer', backgroundColor: '#E8E8E8'}}>
+                                <TableCell sx={{ width: '10%'}}>{index + 1}</TableCell>
+                                <TableCell sx={{ width: '10%'}}>{player.name}</TableCell>
+                                <TableCell sx={{ width: '30%'}}>{calculateAge(player.birthDate)}</TableCell>
+                                <TableCell sx={{ width: '30%'}}>{Math.floor(player.height / 12) + "' " + player.height % 12 + "\""}</TableCell>
+                                <TableCell sx={{ width: '20%'}}>{player.currentTeam}</TableCell>
+                            </TableRow>
+                            ))}
+                        </TableBody>
+                    </table>
+                </TableContainer>
+
+            </Box>
+
+            <Drawer
+                anchor = "right"
+                open = {Boolean(selectedPlayer)}
+                onClose = {handleClose}
+                PaperProps = {{
+                    sx: {
+                        width: 350,
+                        p: 2,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        height: '100vh',
+                    }
+                }}
+
+            >
+                <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', p: 2}}>
+                    <Typography variant = "h6" component = "div">
+                        {selectedPlayer?.name}
+                    </Typography>
+                    <IconButton onClick = {handleClose}>
+                        <CloseIcon />
+                    </IconButton>
+                </Box>
+
+                <TableContainer component = {Paper}></TableContainer>
+
+                {selectedPlayer && (
+                    <Box sx={{ overflowY: 'auto', flex: 1, p: 2 }}>
+                        <img
+                            src = {selectedPlayer.photoUrl || 'https://via.placeholder.com/300x400?text=No+Photo'}
+                            alt = {selectedPlayer.name}
+                            style = {{width: '50%'}}
+                        />
+
+
+                        {/*this is player personal stats*/}
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\nPlayer Stats'}</strong>
+                        </Typography>
+                        <Typography>
+                            <strong>Age: </strong>
+                            {calculateAge(selectedPlayer.birthDate)}
+                        </Typography>
+                        <Typography>
+                            <strong>Height: </strong>
+                            {Math.floor(selectedPlayer.height / 12) + "' " + (selectedPlayer.height % 12) + '"'}
+                        </Typography>
+                        <Typography>
+                            <strong>Weight: </strong>
+                            {selectedPlayer.weight} lbs
+                        </Typography>
+                        <Typography>
+                            <strong>Team: </strong>
+                            {selectedPlayer.currentTeam}
+                        </Typography>
+                        <Typography>
+                            <strong>Home Country: </strong>
+                            {selectedPlayer.homeCountry}
+                        </Typography>
+
+
+                        {/*this is ranking info*/}
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\nAssessments from Different Outlets'}</strong>
+                        </Typography>
+
+                        {matchingPlayer ? (
+                            <>
+                        <Typography>
+                            {matchingPlayer?.["ESPN Rank"] && (<Typography><strong>ESPN Rank:</strong> {matchingPlayer["ESPN Rank"]}</Typography>)}
+                        </Typography>
+                        <Typography>
+                            {matchingPlayer?.["Sam Vecenie Rank"] && (<Typography><strong>Sam Vecenie Rank:</strong> {matchingPlayer["Sam Vecenie Rank"]}</Typography>)}
+                        </Typography>
+                        <Typography>
+                            {matchingPlayer?.["Kevin O'Connor Rank"] && (<Typography><strong>Kevin O'Connor Rank:</strong> {matchingPlayer["Kevin O'Connor Rank"]}</Typography>)}
+                        </Typography>
+                        <Typography>
+                            {matchingPlayer?.["Kyle Boone Rank"] && (<Typography><strong>Kyle Boone Rank:</strong> {matchingPlayer["Kyle Boone Rank"]}</Typography>)}
+                        </Typography>
+                        <Typography>
+                            {matchingPlayer?.["Gary Parrish Rank"] && (<Typography><strong>Gary Parrish Rank:</strong> {matchingPlayer["Gary Parrish Rank"]}</Typography>)}
+                        </Typography>
+                        <Typography sx ={{whiteSpace: 'pre-line'}}>
+                            {prospectRanking != null && (<Typography sx = {{whiteSpace: 'pre-line'}}>
+                                {'\n'}
+                                <strong>Predicted Draft Spot: </strong>
+                                {prospectRanking <= 1 ? 'Clear #1 Overall'
+                                : prospectRanking <= 5 ? 'Consensus Top 5 Pick'
+                                : prospectRanking <= 20 ? 'First Round Pick Based on Team Need'
+                                : prospectRanking <= 40 ? 'Late First Rounder or Second Rounder'
+                                : 'Late Second Rounder'}
+                            </Typography>
+                            )}
+                        </Typography>
+                        </>
+                            ) : (<Typography> No ranking data available</Typography>)
+                        }
+
+
+                        {/*This is player game stats*/}
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\nSeason Stats'}</strong>
+                        </Typography>
+
+                        {matchingPlayerGS ? (
+                            <>
+                        <Typography>
+                            <strong>Games Played: </strong>
+                            {matchingPlayerGS.GP + " (Started in " + matchingPlayerGS.GS + ")"}
+                        </Typography>
+                        <Typography>
+                            <strong>Minutes Played Per Game: </strong>
+                            {matchingPlayerGS.MP}
+                        </Typography>
+                        <Typography>
+                            <strong>Field Goal Percent: </strong>
+                            {matchingPlayerGS["FG%"] + "%"}
+                        </Typography>
+                        <Typography>
+                            <strong>3 Point Percent: </strong>
+                            {matchingPlayerGS["3P%"] + "%"}
+                        </Typography>
+                        <Typography>
+                            <strong>Free Throw Percent: </strong>
+                            {matchingPlayerGS["FTP"] + "%"}
+                        </Typography>
+
+                        {/*Points, Assists, Rebounds, Blocks, Steals*/}
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\nStats Per Game'}</strong>
+                        </Typography>
+                        <Typography>
+                            <strong>Points: </strong>
+                            {matchingPlayerGS.PTS}
+                        </Typography>
+                        <Typography>
+                            <strong>Assists: </strong>
+                            {matchingPlayerGS.AST}
+                        </Typography>
+                        <Typography>
+                            <strong>Rebounds: </strong>
+                            {matchingPlayerGS.TRB}
+                        </Typography>
+                        <Typography>
+                            <strong>Blocks: </strong>
+                            {matchingPlayerGS.BLK}
+                        </Typography>
+                        <Typography>
+                            <strong>Steals: </strong>
+                            {matchingPlayerGS.STL}
+                        </Typography>
+                        <Typography>
+                            <strong>Turnovers: </strong>
+                            {matchingPlayerGS.TOV}
+                        </Typography>
+                            </>
+                        ) : (<Typography> No game stats available</Typography>)
+                        }
+
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\nScouting Reports'}</strong>
+                        </Typography>
+
+                        {matchingReports.length > 0 ? (
+                            matchingReports.map((report, index) => (
+                                <Box key = {index} sx = {{ mb : 2}}>
+                                    <Typography sx ={{whiteSpace: 'pre-line'}}>
+                                        {'\n'}
+                                        <strong>Scout: </strong>
+                                        {report.scout}
+                                    </Typography>
+                                    <Typography>
+                                        <strong>Report: </strong>
+                                        {report.report}
+                                    </Typography>
+                                </Box>
+                            ))
+                        ) : (<Typography> No scouting reports available</Typography>)
+                        }
+
+                        <Typography sx={{ whiteSpace: 'pre-line', textDecoration: 'underline' }}>
+                            <strong>{'\n\n'}</strong>
+                        </Typography>
+
+
+                    </Box>
+                )}
+            </Drawer>
+        </>
+
+    );
+}
+
+export default App
